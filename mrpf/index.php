3994205c6d9ee8221ce5fba985eef40dde5c75ac
forestmm <79968657+forestmm@users.noreply.github.com>
2021-04-19 14:48:35 +0900

    asdasd
    asdasdsa

---
+++ mrpf/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>박미란 디자인 포트폴리오</title>
+    <link rel="preload" href="./nanumGothic.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/font-face.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+    <div class="page-wrapper">
+        <div class="inner-page-wrapper">
+            <div class="intro">
+                <span class="intro-text"></span> <span class="list-of-snacks">
+                <span class="snack">웹디자이너</span>
+                <span class="snack">퍼블리셔</span>
+                <span class="snack">fish sticks</span>
+                <span class="snack">mayonnaise</span>
+                <span class="snack">sand</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
